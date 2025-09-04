553273e9ef332f44062e89a3ade72e077eb2a387
HalleyWorkspace <linkhalley@comcast.net>
2025-09-03 23:28:00 -0400

    Initial Commit: accept input at command line

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client=genai.Client(api_key=api_key)
+
+
+
+def main():
+    input = sys.argv[1]
+    messages = [
+        types.Content(role="user",parts=[types.Part(text=input)]),
+    ]
+    
+    response=client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()

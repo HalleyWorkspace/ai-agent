b463154bd3c6731bc27afd2681a3871d8fc868ac
HalleyWorkspace <linkhalley@comcast.net>
2025-09-08 16:46:00 -0400

    Added Verbose option

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ def main():
     
     response=client.models.generate_content(model="gemini-2.0-flash-001",contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+
+    if '--verbose' in sys.argv:
+        print(f"User prompt: {input}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
